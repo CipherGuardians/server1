34f1f91105932a31a57cfc315c91021b498e8993
CipherGuardians <91021781+CipherGuardians@users.noreply.github.com>
2025-04-15 21:20:36 +0200

    Update setup-warsaw.sh

--- setup-warsaw.sh
+++ setup-warsaw.sh
@@ -21,7 +21,7 @@ cat > /etc/sing-box/config.json <<EOF
       "listen": "0.0.0.0",
       "listen_port": 1443,
       "method": "2022-blake3-aes-256-gcm",
-      "password": "L/5tZNiZvNMK3YNdGT0PTRbpoNs2nBkCIvCiY8MDoN0=",
+      "password": "2EWbBuY+K2GlueZypO9+zKbVxaikKLO92zCmMVXsxHM=",
       "sniff": false
     }
   ],
@@ -29,10 +29,10 @@ cat > /etc/sing-box/config.json <<EOF
     {
       "type": "shadowsocks",
       "tag": "out-ss",
-      "server": "92.38.190.29",
+      "server": "5.188.38.201",
       "server_port": 2443,
       "method": "2022-blake3-aes-256-gcm",
-      "password": "L/5tZNiZvNMK3YNdGT0PTRbpoNs2nBkCIvCiY8MDoN0="
+      "password": "2EWbBuY+K2GlueZypO9+zKbVxaikKLO92zCmMVXsxHM="
     }
   ]
 }
